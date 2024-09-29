1fe22e9c4773b74b76f02916bb18d8761702675a
ahmedsat <ahmedsat303@gmail.com>
2024-09-29 10:04:28 +0300

    normalize, cross product, nad dot  product

--- v3.go
+++ v3.go
@@ -31,3 +31,24 @@ func (v Vector3) Multiply(scalar float32) Vector3 {
 func (v Vector3) Length() float32 {
 	return float32(math.Sqrt(float64(v[0]*v[0] + v[1]*v[1] + v[2]*v[2])))
 }
+
+func (v Vector3) Normalize() Vector3 {
+	length := v.Length()
+	return Vector3{
+		v[0] / length,
+		v[1] / length,
+		v[2] / length,
+	}
+}
+
+func (v Vector3) Cross(other Vector3) Vector3 {
+	return Vector3{
+		v[1]*other[2] - v[2]*other[1],
+		v[2]*other[0] - v[0]*other[2],
+		v[0]*other[1] - v[1]*other[0],
+	}
+}
+
+func (v Vector3) Dot(other Vector3) float32 {
+	return v[0]*other[0] + v[1]*other[1] + v[2]*other[2]
+}
