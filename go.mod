12bccae75cc21edce18cec035f372e4ddf621af0
ahmedsat <ahmedsat303@gmail.com>
2024-10-13 14:51:26 +0300

    Clamp

--- go.mod
+++ go.mod
@@ -8,4 +8,4 @@ replace github.com/ahmedsat/silah => ../silah
 
 replace github.com/ahmedsat/bayaan => ../bayaan
 
-require github.com/ahmedsat/bayaan v0.0.0-00010101000000-000000000000
+require golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c
--- math.go
+++ math.go
@@ -2,6 +2,8 @@ package madar
 
 import (
 	"math/rand/v2"
+
+	"golang.org/x/exp/constraints"
 )
 
 type Rand rand.Rand
@@ -12,4 +14,16 @@ func NewRand(seed1 uint64, seed2 uint64) Rand {
 
 func (r *Rand) RandFloatRange(min, max float32) float32 {
 	return min + (*rand.Rand)(r).Float32()*(max-min)
+}
+
+// Clamp is a generic function that constrains a value to lie between two defined bounds.
+// It works for any type that supports comparison (integers, floats, etc.).
+func Clamp[T constraints.Ordered](x, min, max T) T {
+	if x < min {
+		return min
+	}
+	if x > max {
+		return max
+	}
+	return x
 }
